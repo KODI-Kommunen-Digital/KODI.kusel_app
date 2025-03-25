ee798492e3ca7c0f168b9886518faaf9c2d66a76
dev6ak <dev6@heidi-app.com>
2025-03-25 16:11:03 +0530

    Rename router

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:kusel/theme_manager/theme_manager_controller.dart';
 
 import 'localization_manager.dart';
-import 'mobile_router.dart';
+import 'app_router.dart';
 
 void main() {
   runApp(ProviderScope(child: MyApp()));
