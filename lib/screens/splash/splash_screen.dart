50abc390a69e546539489b5ff9098630265e8a6b
dev6ak <dev6@heidi-app.com>
2025-03-26 12:53:28 +0530

    #3 Remove event card from splash

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:kusel/common_widgets/common_event_card.dart';
 
 import '../../common_widgets/text_styles.dart';
 import '../../locale/localization_manager.dart';
@@ -24,24 +23,9 @@ class _State extends ConsumerState<SplashScreen> {
               .read(localeManagerProvider.notifier)
               .updateCurrentSelectedLocale(Locale('de'));
         },
-        child:
-        Column(
-          children: [
-            CommonEventCard(
-              imageUrl: 'https://fastly.picsum.photos/id/452/200/200.jpg?hmac=f5vORXpRW2GF7jaYrCkzX3EwDowO7OXgUaVYM2NNRXY',
-              date: '04.09.2024',
-              title: 'Mittelaltermarkt',
-              location: 'Burg Lichtenberg',
-              onTap: () {
-                print("Card tapped");
-              },
-              onFavorite: () {
-                print("Favorite tapped");
-              },
-            ),
-          ],
-        ),
-          )),
+        child:  textBoldMontserrat(text: "Kusel App"),
+
+      )),
     );
   }
 }
