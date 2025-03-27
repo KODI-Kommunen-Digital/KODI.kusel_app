8ac32aed7804cbdc7e3ccb5e6acf084c9b938f26
dev6 <dev6@heidi-app.com>
2025-03-27 14:41:56 +0530

    Added home screen component and Splash Screen.

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -1,9 +1,11 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kusel/screens/splash/splash_screen_provider.dart';
 
-import '../../common_widgets/text_styles.dart';
-import '../../locale/localization_manager.dart';
 
 class SplashScreen extends ConsumerStatefulWidget {
   const SplashScreen({super.key});
@@ -12,20 +14,30 @@ class SplashScreen extends ConsumerStatefulWidget {
   ConsumerState<SplashScreen> createState() => _State();
 }
 
+
 class _State extends ConsumerState<SplashScreen> {
+
+  @override
+  void initState() {
+    Timer(const Duration(milliseconds: 1500), () {
+      ref.read(splashScreenProvider.notifier).navigateToNextScreen(context);
+    });    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: InkWell(
-        onTap: () {
-          ref
-              .read(localeManagerProvider.notifier)
-              .updateCurrentSelectedLocale(Locale('de'));
-        },
-        child:  textBoldMontserrat(text: "Kusel App"),
-
-      )),
+        body: Container(
+          color: Color(0xFF18204F),
+          child: Center(
+              child: Text(
+                "Kusel",
+                style: TextStyle(
+                    fontSize: 40, color: Colors.white, fontWeight: FontWeight.bold),
+              )),
+        )
     );
   }
+
+
 }
