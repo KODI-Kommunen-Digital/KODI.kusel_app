d160581227044405de45bc1051d7279224361ea8
Shubham Singh <singh.shubham@fiftyfivetech.io>
2025-03-27 14:41:56 +0530

    Added home screen component and Splash Screen.

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -1,15 +1,22 @@
 import 'package:go_router/go_router.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kusel/screens/dashboard/dashboard_screen.dart';
 import 'package:kusel/screens/splash/splash_screen.dart';
 
 final mobileRouterProvider = Provider((ref) => GoRouter(routes: goRouteList));
 
 const splashScreenPath = "/";
+const dashboardScreenPath = "/dashboardScreenPath";
 
 List<RouteBase> goRouteList = [
   GoRoute(
       path: splashScreenPath,
       builder: (context, state) {
         return SplashScreen();
-      })
+      }),
+  GoRoute(
+      path: dashboardScreenPath,
+      builder: (context, state) {
+        return DashboardScreen();
+      }),
 ];
