bc1c58c3ed13d1ca999947592f7c6eacc24d124a
dev6ak <dev6@heidi-app.com>
2025-03-28 19:37:50 +0530

    Refactor model class

--- domain/lib/model/request_model/sigin/signin_request_model.dart
+++ domain/lib/model/request_model/sigin/signin_request_model.dart
@@ -1,78 +1,24 @@
 import 'package:core/base_model.dart';
 
-class SignInResponseModel implements BaseModel<SignInResponseModel> {
-  String? status;
-  Data? data;
+class SignInRequestModel implements BaseModel<SignInRequestModel> {
+  String? username;
+  String? password;
 
-  SignInResponseModel({this.status, this.data});
+  SignInRequestModel({this.username, this.password});
 
   @override
-  SignInResponseModel fromJson(Map<String, dynamic> json) {
-    return SignInResponseModel(
-      status: json['status'],
-      data: json['data'] != null ? Data().fromJson(json['data']) : null,
+  SignInRequestModel fromJson(Map<String, dynamic> json) {
+    return SignInRequestModel(
+      username: json['username'],
+      password: json['password'],
     );
   }
 
   @override
   Map<String, dynamic> toJson() {
     return {
-      'status': status,
-      'data': data?.toJson(),
-    };
-  }
-}
-
-class Data implements BaseModel<Data> {
-  String? accessToken;
-  String? refreshToken;
-  int? userId;
-  List<CityUser>? cityUsers;
-
-  Data({this.accessToken, this.refreshToken, this.userId, this.cityUsers});
-
-  @override
-  Data fromJson(Map<String, dynamic> json) {
-    return Data(
-      accessToken: json['accessToken'],
-      refreshToken: json['refreshToken'],
-      userId: json['userId'],
-      cityUsers: (json['cityUsers'] as List<dynamic>?)
-          ?.map((e) => CityUser().fromJson(e))
-          .toList(),
-    );
-  }
-
-  @override
-  Map<String, dynamic> toJson() {
-    return {
-      'accessToken': accessToken,
-      'refreshToken': refreshToken,
-      'userId': userId,
-      'cityUsers': cityUsers?.map((e) => e.toJson()).toList(),
-    };
-  }
-}
-
-class CityUser implements BaseModel<CityUser> {
-  int? cityId;
-  int? cityUserId;
-
-  CityUser({this.cityId, this.cityUserId});
-
-  @override
-  CityUser fromJson(Map<String, dynamic> json) {
-    return CityUser(
-      cityId: json['cityId'],
-      cityUserId: json['cityUserId'],
-    );
-  }
-
-  @override
-  Map<String, dynamic> toJson() {
-    return {
-      'cityId': cityId,
-      'cityUserId': cityUserId,
+      'username': username,
+      'password': password,
     };
   }
 }
