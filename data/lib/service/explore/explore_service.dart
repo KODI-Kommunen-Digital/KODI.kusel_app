aff39e2a26466539a1d8a3c59b72a50774a30d36
rhm-heidi <rehman@heidi-app.com>
2025-03-28 18:41:06 +0530

    Explore Screen UI Explore Screen API call Shared pref helper

---
+++ data/lib/service/explore/explore_service.dart
@@ -0,0 +1,23 @@
+import 'package:core/base_model.dart';
+import 'package:dartz/dartz.dart';
+import 'package:data/dio_helper_object.dart';
+import 'package:data/end_points.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:network/src/api_helper.dart';
+
+final exploreServiceProvider = Provider(
+    (ref) => ExploreService(apiHelper: ApiHelper(dioHelper: dioHelperObject)));
+
+class ExploreService {
+  ApiHelper apiHelper;
+
+  ExploreService({required this.apiHelper});
+
+  Future<Either<Exception, BaseModel>> call(
+      BaseModel requestModel, BaseModel responseModel) async {
+    final result = await apiHelper.getRequest(
+        path: exploreEndpoint, create: () => responseModel);
+
+    return result.fold((l) => Left(l), (r) => Right(r));
+  }
+}
