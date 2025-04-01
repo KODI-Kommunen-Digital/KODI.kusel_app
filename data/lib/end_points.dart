5f2e85cbe03c5b40dacc5343fa0e2b88aafec8bb
dev6 <dev6@heidi-app.com>
2025-04-01 18:34:35 +0530

    integrate forgot password api and ui

--- data/lib/end_points.dart
+++ data/lib/end_points.dart
@@ -2,3 +2,4 @@ const baseUrl = "http://116.202.186.81:3001";
 const sigInEndPoint = "/users/login";
 const exploreEndpoint = "/categories";
 const signUpEndPoint = "/users/register";
+const forgotPasswordEndPoint = "/users/forgotPassword";
