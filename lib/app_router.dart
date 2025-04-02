83907e7c1ece98abebdd8aac8f1faf5762210481
dev6 <dev6@heidi-app.com>
2025-04-02 17:02:26 +0530

    implemented sub category screen and api

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -5,6 +5,8 @@ import 'package:kusel/screens/auth/signin/signin_screen.dart';
 import 'package:kusel/screens/auth/signup/signup_screen.dart';
 import 'package:kusel/screens/dashboard/dashboard_screen.dart';
 import 'package:kusel/screens/splash/splash_screen.dart';
+import 'package:kusel/screens/sub_category/sub_category_screen.dart';
+import 'package:kusel/screens/sub_category/sub_category_screen_parameter.dart';
 
 final mobileRouterProvider = Provider((ref) => GoRouter(routes: goRouteList));
 
@@ -13,6 +15,7 @@ const signInScreenPath = "/signInScreen";
 const signUpScreenPath = "/signUpScreen";
 const dashboardScreenPath = "/dashboardScreenPath";
 const forgotPasswordPath = "/forgotPasswordPath";
+const subCategoryScreenPath = "/subCategoryPath";
 
 List<RouteBase> goRouteList = [
   GoRoute(
@@ -40,4 +43,11 @@ List<RouteBase> goRouteList = [
       builder: (context, state) {
         return ForgotPasswordScreen();
       }),
+  GoRoute(
+      path: subCategoryScreenPath,
+      builder: (context, state) {
+        return SubCategoryScreen(
+            subCategoryScreenParameters:
+                state.extra as SubCategoryScreenParameters);
+      })
 ];
