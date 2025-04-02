a4971eefc15668f742b9edf840724a118f7dd9d6
dev6ak <dev6@heidi-app.com>
2025-04-02 19:43:42 +0530

    Added listings and highlight api code.

--- lib/screens/home/home_screen_provider.dart
+++ lib/screens/home/home_screen_provider.dart
@@ -1,14 +1,55 @@
+import 'package:domain/model/response_model/listings_model/get_all_listings_response_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:domain/usecase/listings/listings_usecase.dart';
 
 import 'home_screen_state.dart';
+import 'package:domain/model/request_model/listings/get_all_listings_request_model.dart';
 
 final homeScreenProvider =
     StateNotifierProvider.autoDispose<HomeScreenProvider, HomeScreenState>(
-        (ref) => HomeScreenProvider());
+        (ref) => HomeScreenProvider(listingsUseCase: ref.read(listingsUseCaseProvider)));
 
 class HomeScreenProvider extends StateNotifier<HomeScreenState> {
-  HomeScreenProvider() : super(HomeScreenState.empty());
+  ListingsUseCase listingsUseCase;
+  HomeScreenProvider({required this.listingsUseCase}) : super(HomeScreenState.empty());
+
+  Future<void> getListings() async {
+    try {
+      state = state.copyWith(loading: true, error: "");
+
+      GetAllListingsRequestModel getAllListingsRequestModel = GetAllListingsRequestModel(
+        pageNo: 1,
+        pageSize: 10,
+        sortByStartDate: true,
+        statusId: "active",
+        categoryId: "41",
+        subcategoryId: "456",
+        cityId: "789",
+        translate: "en",
+        startAfterDate: "2024-01-01",
+        endBeforeDate: "2024-12-31",
+      );
+
+      GetAllListingsResponseModel getAllListingsResponseModel =
+      GetAllListingsResponseModel();
+      final result = await listingsUseCase.call(
+          getAllListingsRequestModel, getAllListingsResponseModel);
+      result.fold(
+            (l) {
+          state = state.copyWith(loading: false, error: l.toString());
+        },
+            (r) {
+          var listings = (r as GetAllListingsResponseModel).data;
+          state = state.copyWith(listings: listings, loading: false);
+        },
+      );
+    } catch (error) {
+      state = state.copyWith(loading: false, error: error.toString());
+    }
+  }
+
+
 
   addCarouselListener(CarouselController carouselController) {
     final position = carouselController.position;
