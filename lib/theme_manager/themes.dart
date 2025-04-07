9e04267dd615e072d21e5b86764fdd0ddc147b2e
dev6ak <dev6@heidi-app.com>
2025-04-07 19:15:19 +0530

    Colors updated on app theme.

--- lib/theme_manager/themes.dart
+++ lib/theme_manager/themes.dart
@@ -5,13 +5,34 @@ import 'colors.dart';
 enum Themes { light, dark }
 
 final lightTheme = ThemeData(
-    useMaterial3: false,
-    scaffoldBackgroundColor: lightThemeScaffoldColor,
-    textTheme: TextTheme(
-        displayMedium: TextStyle(color: lightThemeDisplayMediumTextColor),
-        displayLarge: TextStyle(color: lightThemeDisplayLargeTextColor),
-        displaySmall: TextStyle(color: lightThemeDisplaySmallTextColor)),
-    cardColor: lightThemeContainerColor,
+  useMaterial3: false,
+  scaffoldBackgroundColor: lightThemeScaffoldColor,
+  primaryColor: lightThemePrimaryColor,
+  canvasColor: lightThemeWhiteColor,
+  indicatorColor: lightThemeSelectedItemColor,
+  colorScheme: ColorScheme(
+      brightness: Brightness.light,
+      primary: lightThemePrimaryColor,
+      onPrimary: lightThemeFeedbackCardColor,
+      secondary: lightThemeSecondaryColor,
+      onSecondary: lightThemeScaffoldBackgroundColor,
+      error: lightThemeErrorToastColor,
+      onError: lightThemeErrorToastColor,
+      surface: lightThemeCalendarIconColor,
+      onSurface: lightThemeSuccessToastColor,
+      onTertiaryFixed: lightThemeMapMarkerColor),
+  textTheme: TextTheme(
+      displayMedium: TextStyle(color: lightThemeDisplayMediumTextColor),
+      displayLarge: TextStyle(color: lightThemeDisplayLargeTextColor),
+      displaySmall: TextStyle(color: lightThemeDisplaySmallTextColor),
+      labelLarge: TextStyle(color: lightThemeSecondaryColor),
+      labelMedium: TextStyle(color: lightThemeTemperatureColor)),
+  cardTheme: CardTheme(
+    color: lightThemeCardGreyColor,
+  ),
+  hintColor: lightThemeHintColor,
+  dividerColor: lightThemeDividerColor,
+  cardColor: lightThemeContainerColor,
 );
 
 final darkTheme =
