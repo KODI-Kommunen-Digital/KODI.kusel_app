7365fc70dc8c9f5b23b66ad2e80018d28c866cab
Shubham Singh <singh.shubham@fiftyfivetech.io>
2025-04-11 14:29:58 +0530

    Added login signup button on home screen and settings

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -6,6 +6,7 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kusel/app_router.dart';
 import 'package:kusel/navigation/navigation.dart';
+import 'package:kusel/screens/home/home_screen_provider.dart';
 import 'package:kusel/screens/splash/splash_screen_provider.dart';
 import 'package:kusel/theme_manager/colors.dart';
 
@@ -16,47 +17,35 @@ class SplashScreen extends ConsumerStatefulWidget {
   ConsumerState<SplashScreen> createState() => _State();
 }
 
-
 class _State extends ConsumerState<SplashScreen> {
-
+  @override
   @override
   void initState() {
+    super.initState();
+
     Future.microtask(() {
-      ref.read(splashScreenProvider.notifier).startTimer((bool isLogin) {
-
-        String path = "";
-        if(isLogin)
-          {
-                path = dashboardScreenPath;
-          }else{
-               path = signInScreenPath;
-        }
-
+      ref.read(splashScreenProvider.notifier).startTimer(() {
         ref.read(navigationProvider).removeAllAndNavigate(
-            context: context, path: path);
-
+          context: context,
+          path: dashboardScreenPath,
+        );
       });
     });
-
-    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Container(
-          color: Theme.of(context).colorScheme.secondary,
-          child: Center(
-              child: Text(
-                AppLocalizations.of(context).app_title,
-                style: TextStyle(
-                    fontSize: 40,
-                    color:  Theme.of(context).canvasColor,
-                    fontWeight: FontWeight.bold),
-              )),
-        )
-    );
+      color: Theme.of(context).colorScheme.secondary,
+      child: Center(
+          child: Text(
+        AppLocalizations.of(context).app_title,
+        style: TextStyle(
+            fontSize: 40,
+            color: Theme.of(context).canvasColor,
+            fontWeight: FontWeight.bold),
+      )),
+    ));
   }
-
-
 }
