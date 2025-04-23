ac30d233431270a15d932d0f6d6969242bba2ba1
dev6 <dev6@heidi-app.com>
2025-04-23 11:22:38 +0530

    started working on location screen

--- lib/screens/location/location_screen.dart
+++ lib/screens/location/location_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
+import 'package:latlong2/latlong.dart';
 
 class LocationScreen extends ConsumerStatefulWidget {
   const LocationScreen({super.key});
@@ -11,7 +15,50 @@ class LocationScreen extends ConsumerStatefulWidget {
 class _ExploreScreenState extends ConsumerState<LocationScreen> {
   @override
   Widget build(BuildContext context) {
-    return Text("location screen");
+    return SafeArea(
+      child: Scaffold(
+        body: _buildBody(context),
+      ),
+    );
+  }
+
+  _buildBody(BuildContext context) {
+    return SlidingUpPanel(
+      body: FlutterMap(options: MapOptions(
+        onTap: (tapPosition, LatLng latLong) {
+
+        },
+        initialCenter: LatLng(49.53838,7.40647 ),
+        initialZoom: 13.0,
+        interactionOptions: InteractionOptions(),
+      ), children: [
+        TileLayer(
+          urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+          subdomains: ['a', 'b', 'c'],
+        ),
+        MarkerLayer(
+          markers: [
+            Marker(
+              width: 35.w,
+              height: 35.h,
+              point: LatLng(49.53838,7.40647),
+              child: Icon(Icons.location_pin, color: Theme.of(context).colorScheme.onTertiaryFixed),
+            ),
+
+            Marker(
+              width: 35.w,
+              height: 35.h,
+              point: LatLng(49.53348,7.40647),
+              child: Icon(Icons.location_pin, color: Theme.of(context).colorScheme.onTertiaryFixed),
+            )
+          ],
+        )
+      ],),
+      panelBuilder: (controller){
+        return Center(
+          child: Text("Hello"),
+        );
+      },
+    );
   }
 }
-
