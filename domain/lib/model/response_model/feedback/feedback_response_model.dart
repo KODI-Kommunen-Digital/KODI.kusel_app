61c86d7d7e51368e1b05575baa067fbdacf19f20
dev6 <dev6@heidi-app.com>
2025-04-29 18:41:56 +0530

    1. implemented usecase, repos and service for get filter and feedback 2. Implement jwt_decoder to check token expiry

---
+++ domain/lib/model/response_model/feedback/feedback_response_model.dart
@@ -0,0 +1,17 @@
+import 'package:core/base_model.dart';
+
+class FeedBackResponseModel implements BaseModel<FeedBackResponseModel>
+{
+  @override
+  FeedBackResponseModel fromJson(Map<String, dynamic> json) {
+    // TODO: implement fromJson
+    throw UnimplementedError();
+  }
+
+  @override
+  Map<String, dynamic> toJson() {
+    // TODO: implement toJson
+    throw UnimplementedError();
+  }
+
+}
---
+++ domain/lib/model/response_model/filter/get_filter_response_model.dart
@@ -0,0 +1,17 @@
+import 'package:core/base_model.dart';
+
+class GetFilterResponseModel implements BaseModel<GetFilterResponseModel>
+{
+  @override
+  GetFilterResponseModel fromJson(Map<String, dynamic> json) {
+    // TODO: implement fromJson
+    throw UnimplementedError();
+  }
+
+  @override
+  Map<String, dynamic> toJson() {
+    // TODO: implement toJson
+    throw UnimplementedError();
+  }
+
+}
