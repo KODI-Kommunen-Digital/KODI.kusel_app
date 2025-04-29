4e3cb0a2ebf1a910dcb1cd03baa5cffe887649a7
dev6ak <dev6@heidi-app.com>
2025-04-29 15:28:22 +0530

    implemented weather api and it's UI

--- data/lib/end_points.dart
+++ data/lib/end_points.dart
@@ -8,3 +8,8 @@ const listingsEndPoint = "/listings";
 const searchEndPoint = "$listingsEndPoint/search";
 const subCategoriesEndPoint = "/subcategories";
 const userDetailsEndPoint = "/users";
+
+
+// WEATHER API
+const weatherEndPoint = "https://api.weatherapi.com/v1/forecast.json";
+const weatherApiKey="2ead327db48b49f28e6134655242706";
