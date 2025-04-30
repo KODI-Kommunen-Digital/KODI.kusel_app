eb8839d41a90a50ee34fbdf7015aa5d75f5399d1
dev6ak <dev6@heidi-app.com>
2025-04-30 13:05:44 +0530

    update getFav method in favotites_list_screen_controller by adding refresh token validity

--- lib/screens/favorite/favorites_list_screen_controller.dart
+++ lib/screens/favorite/favorites_list_screen_controller.dart
@@ -1,49 +1,99 @@
 import 'package:core/preference_manager/preference_constant.dart';
 import 'package:core/preference_manager/shared_pref_helper.dart';
-import 'package:domain/model/request_model/listings/get_all_listings_request_model.dart';
-import 'package:domain/model/response_model/listings_model/get_all_listings_response_model.dart';
-import 'package:domain/usecase/favorites/get_favorites_usecase.dart';
-import 'package:domain/usecase/listings/listings_usecase.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:core/token_status.dart';
 import 'package:domain/model/request_model/favorites/get_favorites_request_model.dart';
-import 'package:kusel/screens/favorite/favorites_list_screen_parameter.dart';
+import 'package:domain/model/request_model/refresh_token/refresh_token_request_model.dart';
+import 'package:domain/model/response_model/listings_model/get_all_listings_response_model.dart';
+import 'package:domain/model/response_model/refresh_token/refresh_token_response_model.dart';
+import 'package:domain/usecase/favorites/get_favorites_usecase.dart';
+import 'package:domain/usecase/refresh_token/refresh_token_usecase.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'favorites_list_screen_state.dart';
 
 final favoritesListScreenProvider = StateNotifierProvider.autoDispose<
-    FavoritesListScreenController,
-    FavoritesListScreenState>(
-        (ref) =>
-        FavoritesListScreenController(
-            getFavoritesUseCaseProvider: ref.read(getFavoritesUseCaseProvider),
-            sharedPreferenceHelper: ref.read(sharedPreferenceHelperProvider)
-        ));
+        FavoritesListScreenController, FavoritesListScreenState>(
+    (ref) => FavoritesListScreenController(
+        getFavoritesUseCaseProvider: ref.read(getFavoritesUseCaseProvider),
+        sharedPreferenceHelper: ref.read(sharedPreferenceHelperProvider),
+        tokenStatus: ref.read(tokenStatusProvider),
+        refreshTokenUseCase: ref.read(refreshTokenUseCaseProvider)));
 
-class FavoritesListScreenController extends StateNotifier<FavoritesListScreenState> {
-  FavoritesListScreenController({required this.getFavoritesUseCaseProvider, required this.sharedPreferenceHelper})
+class FavoritesListScreenController
+    extends StateNotifier<FavoritesListScreenState> {
+  FavoritesListScreenController(
+      {required this.getFavoritesUseCaseProvider,
+      required this.sharedPreferenceHelper,
+      required this.tokenStatus,
+      required this.refreshTokenUseCase})
       : super(FavoritesListScreenState.empty());
   GetFavoritesUseCase getFavoritesUseCaseProvider;
   SharedPreferenceHelper sharedPreferenceHelper;
+  TokenStatus tokenStatus;
+  RefreshTokenUseCase refreshTokenUseCase;
 
   Future<void> getFavoritesList() async {
     try {
       state = state.copyWith(loading: true, error: "");
 
-      GetFavoritesRequestModel getAllListingsRequestModel =
-      GetFavoritesRequestModel( userId: sharedPreferenceHelper.getInt(userIdKey).toString());
-      GetAllListingsResponseModel getAllListResponseModel = GetAllListingsResponseModel();
+      final response = tokenStatus.isAccessTokenExpired();
 
-      final result = await getFavoritesUseCaseProvider.call(
-          getAllListingsRequestModel, getAllListResponseModel);
-      result.fold(
-            (l) {
+      if (response) {
+        final userId = sharedPreferenceHelper.getInt(userIdKey);
+
+        RefreshTokenResponseModel responseModel = RefreshTokenResponseModel();
+        RefreshTokenRequestModel requestModel =
+            RefreshTokenRequestModel(userId: userId?.toString() ?? "");
+
+        final result =
+            await refreshTokenUseCase.call(requestModel, responseModel);
+
+        result.fold((l) {
           state = state.copyWith(loading: false, error: l.toString());
-        },
+        }, (r) async {
+          final res = r as RefreshTokenResponseModel;
+          sharedPreferenceHelper.setString(
+              tokenKey, res.data?.accessToken ?? "");
+          sharedPreferenceHelper.setString(
+              refreshTokenKey, res.data?.refreshToken ?? "");
+
+          GetFavoritesRequestModel getAllListingsRequestModel =
+              GetFavoritesRequestModel(
+                  userId: sharedPreferenceHelper.getInt(userIdKey).toString());
+          GetAllListingsResponseModel getAllListResponseModel =
+              GetAllListingsResponseModel();
+
+          final result = await getFavoritesUseCaseProvider.call(
+              getAllListingsRequestModel, getAllListResponseModel);
+          result.fold(
+            (l) {
+              state = state.copyWith(loading: false, error: l.toString());
+            },
             (r) {
-          var eventsList = (r as GetAllListingsResponseModel).data;
-          state = state.copyWith(list: eventsList, loading: false);
-        },
-      );
+              var eventsList = (r as GetAllListingsResponseModel).data;
+              state = state.copyWith(list: eventsList, loading: false);
+            },
+          );
+        });
+      } else {
+        GetFavoritesRequestModel getAllListingsRequestModel =
+            GetFavoritesRequestModel(
+                userId: sharedPreferenceHelper.getInt(userIdKey).toString());
+        GetAllListingsResponseModel getAllListResponseModel =
+            GetAllListingsResponseModel();
+
+        final result = await getFavoritesUseCaseProvider.call(
+            getAllListingsRequestModel, getAllListResponseModel);
+        result.fold(
+          (l) {
+            state = state.copyWith(loading: false, error: l.toString());
+          },
+          (r) {
+            var eventsList = (r as GetAllListingsResponseModel).data;
+            state = state.copyWith(list: eventsList, loading: false);
+          },
+        );
+      }
     } catch (error) {
       state = state.copyWith(loading: false, error: error.toString());
     }
@@ -57,7 +107,8 @@ class FavoritesListScreenController extends StateNotifier<FavoritesListScreenSta
         listing.isFavorite = isFavorite;
       }
     }
-    final updatedList = state.eventsList.where((listing) => listing.id != id).toList();
+    final updatedList =
+        state.eventsList.where((listing) => listing.id != id).toList();
     state = state.copyWith(
       list: updatedList,
     );
