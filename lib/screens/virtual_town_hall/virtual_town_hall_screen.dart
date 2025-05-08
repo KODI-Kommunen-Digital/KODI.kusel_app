dd8efc8092e1b54d17c88038145e34dbf1b288e9
dev6ak <dev6@heidi-app.com>
2025-05-08 12:38:41 +0530

    updated signup controller and updated explore screen with the new ui i.e; virtual town hall, my town etc

---
+++ lib/screens/virtual_town_hall/virtual_town_hall_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class VirtualTownHallScreen extends ConsumerStatefulWidget {
+  const VirtualTownHallScreen({super.key});
+
+  @override
+  ConsumerState<VirtualTownHallScreen> createState() => _VirtualTownHallScreenState();
+}
+
+class _VirtualTownHallScreenState extends ConsumerState<VirtualTownHallScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
