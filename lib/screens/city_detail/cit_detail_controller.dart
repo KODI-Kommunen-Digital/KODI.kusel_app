fca91445be2e1f58dbba12cc31f564ca8362e0d4
NamanFiftyFive <dev2@heidi-app.com>
2025-05-08 15:35:20 +0530

    refactor explore screen

---
+++ lib/screens/city_detail/cit_detail_controller.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kusel/screens/city_detail/city_detail_state.dart';
+
+class CityDetailController extends StateNotifier<CityDetailState> {
+  CityDetailController() : super(CityDetailState.empty());
+
+
+}
