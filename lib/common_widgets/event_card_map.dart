f592bebd45a6d013d754de2af933ea9aedcba650
dev6ak <dev6@heidi-app.com>
2025-05-08 15:06:57 +0530

    Bug Fixes

--- lib/common_widgets/event_card_map.dart
+++ lib/common_widgets/event_card_map.dart
@@ -1,4 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:domain/model/response_model/listings_model/get_all_listings_response_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
