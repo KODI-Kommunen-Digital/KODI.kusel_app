362776542c74a726ded03f0936f7802a6c33a156
dev6ak <dev6@heidi-app.com>
2025-05-12 15:27:54 +0530

    Added mobility and participate screen. And fixed tab view ui issues.

--- lib/screens/municipal_party_detail/municipal_detail_controller.dart
+++ lib/screens/municipal_party_detail/municipal_detail_controller.dart
@@ -1,7 +1,10 @@
+import 'package:domain/model/request_model/city_details/get_city_details_request_model.dart';
 import 'package:domain/model/request_model/listings/get_all_listings_request_model.dart';
 import 'package:domain/model/request_model/municipal_party_detail/municipal_party_detail_request_model.dart';
+import 'package:domain/model/response_model/city_details/get_city_details_response_model.dart';
 import 'package:domain/model/response_model/listings_model/get_all_listings_response_model.dart';
 import 'package:domain/model/response_model/municipal_party_detail/municipal_party_detail_response_model.dart';
+import 'package:domain/usecase/city_details/get_city_details_usecase.dart';
 import 'package:domain/usecase/listings/listings_usecase.dart';
 import 'package:domain/usecase/municipal_party_detail/municipal_party_detail_use_case.dart';
 import 'package:flutter/material.dart';
@@ -14,21 +17,25 @@ final municipalDetailControllerProvider = StateNotifierProvider.autoDispose<
     (ref) => MunicipalDetailController(
         listingsUseCase: ref.read(listingsUseCaseProvider),
         municipalPartyDetailUseCase:
-            ref.read(municipalPartyDetailUseCaseProvider)));
+            ref.read(municipalPartyDetailUseCaseProvider),
+      getCityDetailsUseCase: ref.read(getCityDetailsUseCaseProvider)
+    ));
 
 class MunicipalDetailController extends StateNotifier<MunicipalDetailState> {
   ListingsUseCase listingsUseCase;
   MunicipalPartyDetailUseCase municipalPartyDetailUseCase;
+  GetCityDetailsUseCase getCityDetailsUseCase;
 
   MunicipalDetailController(
       {required this.listingsUseCase,
-      required this.municipalPartyDetailUseCase})
+      required this.municipalPartyDetailUseCase,
+      required this.getCityDetailsUseCase})
       : super(MunicipalDetailState.empty());
 
   getEventsUsingCityId({required String municipalId}) async {
     try {
       state = state.copyWith(showEventLoading: true);
-      final id = "1"??municipalId;
+      final id = "1" ?? municipalId;
       GetAllListingsRequestModel requestModel =
           GetAllListingsRequestModel(cityId: id, categoryId: '3');
 
@@ -53,7 +60,7 @@ class MunicipalDetailController extends StateNotifier<MunicipalDetailState> {
   getNewsUsingCityId({required String municipalId}) async {
     try {
       state = state.copyWith(showNewsLoading: true);
-      final id = "1"??municipalId;
+      final id = "1" ?? municipalId;
       final categoryId = "1";
       GetAllListingsRequestModel requestModel =
           GetAllListingsRequestModel(cityId: id);
@@ -100,6 +107,37 @@ class MunicipalDetailController extends StateNotifier<MunicipalDetailState> {
     } catch (e) {
       state = state.copyWith(isLoading: false);
       debugPrint("getMunicipalPartyDetailUsingId exception = $e");
+    }
+  }
+
+  Future<void> fetchCities() async {
+    try {
+      GetCityDetailsRequestModel requestModel =
+          GetCityDetailsRequestModel(hasForum: false);
+      GetCityDetailsResponseModel responseModel = GetCityDetailsResponseModel();
+      final result =
+          await getCityDetailsUseCase.call(requestModel, responseModel);
+
+      result.fold((l) {
+        debugPrint('get city details fold exception : $l');
+      }, (r) async {
+        final response = r as GetCityDetailsResponseModel;
+
+        // final cityDetailsMap = <int, String>{};
+        //
+        // if (response.data != null) {
+        //   for (var city in response.data!) {
+        //     if (city.id != null && city.name != null) {
+        //       cityDetailsMap[city.id!] = city.name!;
+        //     }
+        //   }
+        // }
+        state = state.copyWith(
+            cityList: response.data,
+        );
+      });
+    } catch (error) {
+      debugPrint('get city details exception : $error');
     }
   }
 }
