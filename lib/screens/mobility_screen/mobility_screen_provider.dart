7b5e6b546971880726f6237f472ce5d5b4943379
dev6ak <dev6@heidi-app.com>
2025-05-15 15:42:34 +0530

    Mobility and participate screen api impl.

--- lib/screens/mobility_screen/mobility_screen_provider.dart
+++ lib/screens/mobility_screen/mobility_screen_provider.dart
@@ -1,10 +1,46 @@
+import 'package:domain/model/empty_request.dart';
+import 'package:domain/model/response_model/mobility/mobility_response_model.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kusel/screens/mobility_screen/mobility_screen_state.dart';
+import 'package:domain/usecase/mobility/mobility_usecase.dart';
 
 final mobilityScreenProvider = StateNotifierProvider.autoDispose<
-    MobilityScreenProvider,
-    MobilityScreenState>((ref) => MobilityScreenProvider());
+        MobilityScreenProvider, MobilityScreenState>(
+    (ref) => MobilityScreenProvider(
+        mobilityUseCase: ref.read(mobilityUseCaseProvider)));
 
 class MobilityScreenProvider extends StateNotifier<MobilityScreenState> {
-  MobilityScreenProvider() : super(MobilityScreenState.empty());
+  MobilityUseCase mobilityUseCase;
+
+  MobilityScreenProvider({required this.mobilityUseCase})
+      : super(MobilityScreenState.empty());
+
+  Future<void> fetchMobilityDetails() async {
+    try {
+      state = state.copyWith(isLoading: true);
+
+      EmptyRequest requestModel = EmptyRequest();
+
+      MobilityResponseModel responseModel = MobilityResponseModel();
+
+      final response = await mobilityUseCase.call(requestModel, responseModel);
+
+      response.fold((l) {
+        state = state.copyWith(isLoading: false);
+        debugPrint("Mobility fold exception = ${l.toString()}");
+      }, (r) {
+        final result = r as MobilityResponseModel;
+        if (result.data != null) {
+          state = state.copyWith(
+            mobilityData: result.data,
+            isLoading: false,
+          );
+        }
+      });
+    } catch (e) {
+      state = state.copyWith(isLoading: false);
+      debugPrint("Mobility Ort exception = $e");
+    }
+  }
 }
