bef7158557d7318b1f3f4f432bf477b784f92695
dev6ak <dev6@heidi-app.com>
2025-06-20 13:21:42 +0530

    updatae localization.yaml file

---
+++ lib/l10n/app_localizations_en.dart
@@ -0,0 +1,616 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get welcome => 'Welcome to Flutter Localization';
+
+  @override
+  String get app_title => 'Heidi';
+
+  @override
+  String get enter_email_id => 'Email or Username';
+
+  @override
+  String get password => 'Password';
+
+  @override
+  String get forgot_password => 'Forgot Password?';
+
+  @override
+  String get email => 'Email';
+
+  @override
+  String get login => 'Login';
+
+  @override
+  String get signup => 'SignUp';
+
+  @override
+  String get or => 'Or';
+
+  @override
+  String get username => 'Username';
+
+  @override
+  String get firstName => 'First Name';
+
+  @override
+  String get lastName => 'Last Name';
+
+  @override
+  String get roleId => 'Role ID';
+
+  @override
+  String get description => 'Description';
+
+  @override
+  String get submit => 'Submit';
+
+  @override
+  String get forgot => 'Forgot Password';
+
+  @override
+  String get email_send => 'Email has been send';
+
+  @override
+  String get something_went_wrong => 'Something went wrong, Please try again later';
+
+  @override
+  String get highlights => 'Highlights';
+
+  @override
+  String get category_heading => 'What is there to discover';
+
+  @override
+  String get change_language => 'Change Language';
+
+  @override
+  String get logout => 'Logout';
+
+  @override
+  String get select_language => 'Select your language';
+
+  @override
+  String get medieval_market => 'Medieval market';
+
+  @override
+  String get public_transport_offer => 'Public transport offer';
+
+  @override
+  String get next_dates => 'Next dates';
+
+  @override
+  String get search => 'Search';
+
+  @override
+  String get enter_search_term => 'Enter search term';
+
+  @override
+  String get feedback_heading => 'What do you think of the app?';
+
+  @override
+  String get feedback_description => 'Is there anything missing or have you noticed something? Let us know so we can continually improve the app.';
+
+  @override
+  String get send_feedback => 'Send feedback';
+
+  @override
+  String get no_data => 'No data';
+
+  @override
+  String get events => 'Events';
+
+  @override
+  String get all_events => 'View all events';
+
+  @override
+  String get settings => 'Settings';
+
+  @override
+  String get reset => 'Reset';
+
+  @override
+  String get period => 'Period';
+
+  @override
+  String get target_group => 'Target Group';
+
+  @override
+  String get ort => 'Location';
+
+  @override
+  String get perimeter => 'Perimeter';
+
+  @override
+  String get sort_by => 'Sort by';
+
+  @override
+  String get actuality => 'Actuality';
+
+  @override
+  String get distance => 'Distance';
+
+  @override
+  String get cancel => 'Cancel';
+
+  @override
+  String get options => 'Options';
+
+  @override
+  String get apply => 'Apply';
+
+  @override
+  String get log_in_sign_up => 'SIGN UP | LOG IN';
+
+  @override
+  String get near_you => 'Near you';
+
+  @override
+  String get to_map_view => 'To map view';
+
+  @override
+  String get map_fav => 'Favoriten';
+
+  @override
+  String get map_calendar => 'Event';
+
+  @override
+  String get map_restaurant => 'Gastronomy';
+
+  @override
+  String get select_environment => 'Select Environment';
+
+  @override
+  String get search_heading => 'How can I help you?\nJust ask me anything.';
+
+  @override
+  String get near_me => 'What\'s near me';
+
+  @override
+  String get recommendations => 'Recommendations';
+
+  @override
+  String get recent_search => 'Recent Searches';
+
+  @override
+  String get search_result => 'Search Results';
+
+  @override
+  String get check_your_email => 'A link has been send to your email, please verify';
+
+  @override
+  String get lets_get_started => 'Let\'s get started';
+
+  @override
+  String get next => 'Next';
+
+  @override
+  String get another_time => 'Another time';
+
+  @override
+  String get back => 'Back';
+
+  @override
+  String get skip => 'Skip';
+
+  @override
+  String get what_may_i_call_you => 'What may I call you?';
+
+  @override
+  String get your_name => 'Your name';
+
+  @override
+  String get ich_text => 'I ...';
+
+  @override
+  String get i_live_in_district => 'I live in the district...';
+
+  @override
+  String get welcome_text => 'Welcome!';
+
+  @override
+  String get welcome_para_first => 'In order to offer you tailored content from the region that will also interest you, we would need a little information.';
+
+  @override
+  String get welcome_para_second => 'This information is voluntary and can be skipped. You can adjust it at any time in the settings.';
+
+  @override
+  String get live_here => 'live in';
+
+  @override
+  String get spend_my_free_time_here => 'Spend my free time in';
+
+  @override
+  String get your_place_of_residence => 'Your place of residence';
+
+  @override
+  String get select_residence => 'Select residence';
+
+  @override
+  String get alone => 'Alone';
+
+  @override
+  String get for_two => 'For two';
+
+  @override
+  String get with_dog => 'With dog';
+
+  @override
+  String get barrierearm => 'Barrierearm';
+
+  @override
+  String get with_my_family => 'With my family';
+
+  @override
+  String get your_location => 'Your location (optional)';
+
+  @override
+  String get almost_there => 'Almost there, ';
+
+  @override
+  String get what_interest_you => '! What interests you in the district?';
+
+  @override
+  String get complete => 'Complete';
+
+  @override
+  String get preparing_the_app_text => 'I\'m now preparing the app for you.\nThis will take a moment.';
+
+  @override
+  String get i_have_prepared_everything_text => 'I\'ve prepared everything for you.\nHave fun!';
+
+  @override
+  String get continue_to_homepage => 'Continue to the homepage';
+
+  @override
+  String get ready => 'Ready';
+
+  @override
+  String get thanks => 'Thanks';
+
+  @override
+  String get name => 'Name';
+
+  @override
+  String get details => 'Details';
+
+  @override
+  String get profile => 'Profile';
+
+  @override
+  String get field_updated_message => 'Field Updated Successfully';
+
+  @override
+  String get enter_name => 'Enter Name';
+
+  @override
+  String get enter_username => 'Enter Username';
+
+  @override
+  String get enter_email => 'Enter Email';
+
+  @override
+  String get phone_number => 'Phone Number';
+
+  @override
+  String get enter_phone_number => 'Enter Phone Number';
+
+  @override
+  String get enter_description => 'Enter Description';
+
+  @override
+  String get website => 'Website';
+
+  @override
+  String get enter_website => 'Enter Website';
+
+  @override
+  String get save_changes => 'Save Changes';
+
+  @override
+  String get edit => 'Edit';
+
+  @override
+  String get favorites => 'Favorites';
+
+  @override
+  String get please_select_the_field => 'Please complete the form';
+
+  @override
+  String get title => 'Title';
+
+  @override
+  String get feedback => 'Feedback';
+
+  @override
+  String get enter_title => 'Enter Title';
+
+  @override
+  String get feedback_sent_successfully => 'Feedback sent successfully';
+
+  @override
+  String get barrier_free => 'Barrier-free';
+
+  @override
+  String get dogs_allow => 'Dogs allowed';
+
+  @override
+  String get today => 'Today';
+
+  @override
+  String get weekend => 'Weekend';
+
+  @override
+  String get next_7_days => 'Next 7 days';
+
+  @override
+  String get next_30_days => 'Next 30 days';
+
+  @override
+  String get define_period => 'Define period';
+
+  @override
+  String get as_a_couple => 'As a couple';
+
+  @override
+  String get with_children => 'With children';
+
+  @override
+  String get groups => 'Groups';
+
+  @override
+  String get seniors => 'Senior citizen';
+
+  @override
+  String get from => 'From';
+
+  @override
+  String get to => 'To';
+
+  @override
+  String get select_start_date => 'Select start date';
+
+  @override
+  String get select_end_date => 'Select end date';
+
+  @override
+  String get please_select_start_and_end_date => 'Please select start and end date';
+
+  @override
+  String get today_its_going_to_be => 'It’s going to be sunny today!';
+
+  @override
+  String get virtual_town_hall => 'Virtual Town Hall';
+
+  @override
+  String get my_town => 'Hometown';
+
+  @override
+  String get tourism_and_leisure => 'Tourism and Leisure';
+
+  @override
+  String get mobility => 'Mobility';
+
+  @override
+  String get get_involved => 'GET INVOLVED';
+
+  @override
+  String get sunny => 'Sunny';
+
+  @override
+  String get find_out_how_to => 'Find out how to get there here.';
+
+  @override
+  String get read_more => 'Read more';
+
+  @override
+  String get places_of_the_community => 'Places of the community';
+
+  @override
+  String get show_all_locations => 'Show all locations';
+
+  @override
+  String get all_news => 'View all news';
+
+  @override
+  String get municipality => 'municipality';
+
+  @override
+  String get open => 'Open';
+
+  @override
+  String get close => 'Close';
+
+  @override
+  String get visit_website => 'Visit website';
+
+  @override
+  String get associated_municipalities => 'Associated municipalities';
+
+  @override
+  String get news => 'News';
+
+  @override
+  String get event_text => 'Events';
+
+  @override
+  String get feedback_text => 'I have read the privacy policy';
+
+  @override
+  String get imprint_page => 'Imprint Page';
+
+  @override
+  String get terms_of_use => 'Terms of Use';
+
+  @override
+  String get privacy_policy => 'Privacy Policy';
+
+  @override
+  String get view_ort => 'View Ort';
+
+  @override
+  String get soon_service => 'Soon service will be availbale';
+
+  @override
+  String get cities => 'Cities';
+
+  @override
+  String get my_place => 'Hometown';
+
+  @override
+  String get show_all_places => 'Show all places';
+
+  @override
+  String get mein_ort_display_message => 'Welcome to the overview page for your places in the Kuseler Land – your guide to the places in the region.';
+
+  @override
+  String get hiking_trails => 'Hiking trails app';
+
+  @override
+  String get discover_kusel_on_foot => 'Discover Kusel on foot';
+
+  @override
+  String get our_offers => 'Our offers';
+
+  @override
+  String get your_contact_persons => 'Your contact persons';
+
+  @override
+  String get participate => 'PARTICIPATE';
+
+  @override
+  String get register_here => 'Register here';
+
+  @override
+  String get develop_kusel_together_text => 'We want to further develop the Kusel district together.';
+
+  @override
+  String get sunday_text => 'Su';
+
+  @override
+  String get monday_text => 'Mo';
+
+  @override
+  String get tuesday_text => 'Tu';
+
+  @override
+  String get wednesday_text => 'We';
+
+  @override
+  String get thursday_text => 'Th';
+
+  @override
+  String get friday_text => 'Fr';
+
+  @override
+  String get saturday_text => 'Sa';
+
+  @override
+  String get current_events => 'Current Events';
+
+  @override
+  String get accessible => 'Accessible';
+
+  @override
+  String get reachable_by_public_transport => 'Reachable by public transport';
+
+  @override
+  String get free_of_charge => 'Free of charge';
+
+  @override
+  String get bookable_online => 'Bookable online';
+
+  @override
+  String get open_air => 'Open air';
+
+  @override
+  String get card_payment_possible => 'Card payment possible';
+
+  @override
+  String get is_required => 'is Required';
+
+  @override
+  String get required => 'Required';
+
+  @override
+  String get name_or_email => 'Name or Email';
+
+  @override
+  String get kusel => 'Kusel';
+
+  @override
+  String get district => 'District';
+
+  @override
+  String get favourite_city => 'Favourite cities';
+
+  @override
+  String get delete_account => 'Delete account';
+
+  @override
+  String get success_delete_account_message => 'You account has been deleted successfully';
+
+  @override
+  String get edit_onboarding_details => 'Edit onboarding details';
+
+  @override
+  String get ok => 'Ok';
+
+  @override
+  String get delete_account_information => 'Are you sure you want to delete your account?';
+
+  @override
+  String get digifit_parcours => 'Digifit Parcours';
+
+  @override
+  String get scan_exercise => 'Scan exercise';
+
+  @override
+  String get points => 'Points';
+
+  @override
+  String get trophies => 'Trophies';
+
+  @override
+  String get brain_teasers => 'Brain Teasers';
+
+  @override
+  String get points_and_trophy => 'Points & Trophies';
+
+  @override
+  String get show_course => 'Show course';
+
+  @override
+  String get scan_the_qr_text => 'Scan the QR code at the training station';
+
+  @override
+  String get link_copy_to_clipboard => 'Link copied to clipboard';
+
+  @override
+  String get privacy_policy_error_msg => 'Please accept the Privacy Policy to continue.';
+
+  @override
+  String get digifit => 'Digifit';
+
+  @override
+  String get closes_at => 'Closes at';
+
+  @override
+  String get clock => 'Clock';
+
+  @override
+  String get saturday => 'Saturday';
+
+  @override
+  String get people_with_disabilities => 'People with disabilities';
+
+  @override
+  String get select => 'Select';
+}
--- lib/screens/auth/signin/signin_screen.dart
+++ lib/screens/auth/signin/signin_screen.dart
@@ -2,7 +2,7 @@ import 'dart:ui';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:kusel/l10n/app_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:kusel/app_router.dart';
