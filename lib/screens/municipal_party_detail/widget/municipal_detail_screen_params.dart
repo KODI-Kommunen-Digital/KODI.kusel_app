58d69c7aceaf025bee687a78bbdae99574a65391
dev6ak <dev6@heidi-app.com>
2025-06-24 18:10:35 +0530

    update api for onboarding and main ort

--- lib/screens/municipal_party_detail/widget/municipal_detail_screen_params.dart
+++ lib/screens/municipal_party_detail/widget/municipal_detail_screen_params.dart
@@ -1,5 +1,5 @@
 class MunicipalDetailScreenParams{
   String municipalId;
-  Function(bool isFav, int? id, bool? isMunicipal) onFavUpdate;
+  Function(bool isFav, int? id, bool? isMunicipal)? onFavUpdate;
   MunicipalDetailScreenParams({required this.municipalId, required this.onFavUpdate});
 }
