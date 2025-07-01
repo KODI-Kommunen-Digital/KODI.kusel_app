ac7119b54b5e858751be399841701d4a01b55b20
Naman Khanaa <naman.khanaa@fiftyfivetech.io>
2025-07-01 18:13:02 +0530

    feat: handle the refresh token case for the digifit_screens.

--- lib/screens/digifit_screens/digifit_overview/digifit_overview_controller.dart
+++ lib/screens/digifit_screens/digifit_overview/digifit_overview_controller.dart
@@ -1,33 +1,54 @@
-import 'package:core/preference_manager/preference_constant.dart';
-import 'package:core/preference_manager/shared_pref_helper.dart';
+import 'package:core/token_status.dart';
 import 'package:domain/model/request_model/digifit/digifit_overview_request_model.dart';
 import 'package:domain/model/response_model/digifit/digifit_overview_response_model.dart';
 import 'package:domain/usecase/digifit/digifit_overview_usecase.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../../providers/refresh_token_provider.dart';
 import 'digifit_overview_state.dart';
 
 final digifitOverviewScreenControllerProvider = StateNotifierProvider
-    .autoDispose<DigifitOverviewController, DigifitOverviewState>(
-  (ref) => DigifitOverviewController(
-    digifitOverviewUseCase: ref.read(digifitOverviewUseCaseProvider),
-    sharedPreferenceHelper: ref.read(sharedPreferenceHelperProvider),
-  ),
-);
+    .autoDispose<DigifitOverviewController, DigifitOverviewState>((ref) =>
+        DigifitOverviewController(
+            digifitOverviewUseCase: ref.read(digifitOverviewUseCaseProvider),
+            tokenStatus: ref.read(tokenStatusProvider),
+            refreshTokenProvider: ref.read(refreshTokenProvider)));
 
 class DigifitOverviewController extends StateNotifier<DigifitOverviewState> {
   final DigifitOverviewUseCase digifitOverviewUseCase;
-  final SharedPreferenceHelper sharedPreferenceHelper;
+  final TokenStatus tokenStatus;
+  final RefreshTokenProvider refreshTokenProvider;
 
   DigifitOverviewController(
       {required this.digifitOverviewUseCase,
-      required this.sharedPreferenceHelper})
+      required this.tokenStatus,
+      required this.refreshTokenProvider})
       : super(DigifitOverviewState.empty());
 
   Future<void> fetchDigifitOverview(String location) async {
     try {
       state = state.copyWith(isLoading: true);
 
+      final isTokenExpired = tokenStatus.isAccessTokenExpired();
+
+      if (isTokenExpired) {
+        await refreshTokenProvider.getNewToken(
+            onError: () {},
+            onSuccess: () {
+              _fetchDigifitOverview(location);
+            });
+      } else {
+        // If the token is not expired, we can proceed with the request
+        _fetchDigifitOverview(location);
+      }
+    } catch (e) {
+      debugPrint('[DigifitOverviewController] Fetch Exception');
+    }
+  }
+
+  _fetchDigifitOverview(String location) async {
+    try {
       DigifitOverviewRequestModel digifitOverviewRequestModel =
           DigifitOverviewRequestModel(location: location);
 
@@ -37,18 +58,20 @@ class DigifitOverviewController extends StateNotifier<DigifitOverviewState> {
       final result = await digifitOverviewUseCase.call(
           digifitOverviewRequestModel, digifitOverviewResponseModel);
 
-      result.fold((l) {
-        state = state.copyWith(isLoading: false, errorMessage: l.toString());
-      }, (r) {
-        var response = (r as DigifitOverviewResponseModel).data;
-        state = state.copyWith(
-            isLoading: false, digifitOverviewDataModel: response);
-      });
-    } catch (error) {
-      state = state.copyWith(
-        isLoading: false,
-        errorMessage: error.toString(),
+      result.fold(
+        (l) {
+          state = state.copyWith(isLoading: false, errorMessage: l.toString());
+          debugPrint(
+              '[DigifitOverviewController] Fetch fold Error: ${l.toString()}');
+        },
+        (r) {
+          var response = (r as DigifitOverviewResponseModel).data;
+          state = state.copyWith(
+              isLoading: false, digifitOverviewDataModel: response);
+        },
       );
+    } catch (error) {
+      debugPrint('[DigifitOverviewController] Fetch fold Exception: $error');
     }
   }
 }
--- lib/screens/digifit_screens/digifit_start/digifit_information_controller.dart
+++ lib/screens/digifit_screens/digifit_start/digifit_information_controller.dart
@@ -1,25 +1,54 @@
+import 'package:core/token_status.dart';
 import 'package:domain/model/empty_request.dart';
 import 'package:domain/model/response_model/digifit/digifit_information_response_model.dart';
 import 'package:domain/usecase/digifit/digifit_information_usecase.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../../providers/refresh_token_provider.dart';
 import 'digifit_information_state.dart';
 
 final digifitInformationControllerProvider = StateNotifierProvider.autoDispose<
     DigifitInformationController, DigifitState>(
   (ref) => DigifitInformationController(
-      digifitInformationUsecase: ref.read(digifitInformationUseCaseProvider)),
+      digifitInformationUsecase: ref.read(digifitInformationUseCaseProvider),
+      tokenStatus: ref.read(tokenStatusProvider),
+      refreshTokenProvider: ref.read(refreshTokenProvider)),
 );
 
 class DigifitInformationController extends StateNotifier<DigifitState> {
   final DigifitInformationUseCase digifitInformationUsecase;
+  final TokenStatus tokenStatus;
+  final RefreshTokenProvider refreshTokenProvider;
 
-  DigifitInformationController({required this.digifitInformationUsecase})
+  DigifitInformationController(
+      {required this.digifitInformationUsecase,
+      required this.tokenStatus,
+      required this.refreshTokenProvider})
       : super(DigifitState.empty());
 
   Future<void> fetchDigifitInformation() async {
     try {
       state = state.copyWith(isLoading: true);
 
+      final isTokenExpired = tokenStatus.isAccessTokenExpired();
+
+      if (isTokenExpired) {
+        await refreshTokenProvider.getNewToken(
+            onError: () {},
+            onSuccess: () {
+              _fetchDigifitInformation();
+            });
+      } else {
+        // If the token is not expired, we can proceed with the request
+        _fetchDigifitInformation();
+      }
+    } catch (e) {
+      debugPrint('[DigifitInformationController] Fetch Exception: $e');
+    }
+  }
+
+  _fetchDigifitInformation() async {
+    try {
       EmptyRequest emptyRequest = EmptyRequest();
 
       DigifitInformationResponseModel digifitInformationResponseModel =
@@ -33,6 +62,8 @@ class DigifitInformationController extends StateNotifier<DigifitState> {
             isLoading: false,
             errorMessage: l.toString(),
           );
+          debugPrint(
+              '[DigifitInformationController] Fetch Error: ${l.toString()}');
         },
         (r) {
           var response = (r as DigifitInformationResponseModel).data;
@@ -41,10 +72,7 @@ class DigifitInformationController extends StateNotifier<DigifitState> {
         },
       );
     } catch (error) {
-      state = state.copyWith(
-        isLoading: false,
-        errorMessage: error.toString(),
-      );
+      debugPrint('[DigifitInformationController] Fetch fold Exception: $error');
     }
   }
 }
