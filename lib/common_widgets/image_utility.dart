dca5fb53d7b8e102c0a492217c8eaa2aa85a8820
Naman Khanaa <naman.khanaa@fiftyfivetech.io>
2025-07-02 17:23:12 +0530

    fix: remove the println from the imageUtils.

--- lib/common_widgets/image_utility.dart
+++ lib/common_widgets/image_utility.dart
@@ -35,7 +35,6 @@ class ImageUtil {
           );
         },
         errorWidget: (context, value, _) {
-          print('[Infomtation Image] as the 10 as $svgErrorImagePath');
           return (svgErrorImagePath != null)
               ? SvgPicture.asset(svgErrorImagePath)
               : Icon(Icons.broken_image);
