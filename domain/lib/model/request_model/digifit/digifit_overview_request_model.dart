f1f83f16d42a9bcbab003db869e10b9e35bc4931
dev6ak <dev6@heidi-app.com>
2025-07-02 19:57:59 +0530

    feat: Implement logic to handle the translation of the digifit screens.

--- domain/lib/model/request_model/digifit/digifit_overview_request_model.dart
+++ domain/lib/model/request_model/digifit/digifit_overview_request_model.dart
@@ -3,16 +3,26 @@ import 'package:core/base_model.dart';
 class DigifitOverviewRequestModel
     extends BaseModel<DigifitOverviewRequestModel> {
   final int locationId;
+  final String translate;
 
-  DigifitOverviewRequestModel({required this.locationId});
+  DigifitOverviewRequestModel({
+    required this.locationId,
+    required this.translate,
+  });
 
   @override
   DigifitOverviewRequestModel fromJson(Map<String, dynamic> json) {
-    return DigifitOverviewRequestModel(locationId: json['locationId'] ?? '');
+    return DigifitOverviewRequestModel(
+      locationId: json['locationId'] ?? 0,
+      translate: json['translate'] ?? '',
+    );
   }
 
   @override
   Map<String, dynamic> toJson() {
-    return {'locationId': locationId};
+    return {
+      'locationId': locationId,
+      'translate': translate,
+    };
   }
 }
