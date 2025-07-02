5b2b697cf80bd1c843eaf731816c108a19c736ca
NamanFiftyFive <dev2@heidi-app.com>
2025-07-02 19:41:20 +0530

    implement logic to mark digifit quipment fav

---
+++ lib/providers/digifit_equipment_fav_provider.dart
@@ -0,0 +1,87 @@
+import 'package:core/token_status.dart';
+import 'package:domain/model/request_model/digifit/digifit_equipment_fav_request_model.dart';
+import 'package:domain/model/response_model/digifit/digifit_equipment_fav_response_model.dart';
+import 'package:domain/usecase/digifit/digifit_equipment_fav_usecase.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kusel/providers/refresh_token_provider.dart';
+
+final digitEquipmentFavProvider = Provider((ref) => DigifitEquipmentFav(
+    refreshTokenProvider: ref.read(refreshTokenProvider),
+    tokenStatus: ref.read(tokenStatusProvider),
+    digifitEquipmentFavUseCase: ref.read(digifitEquipmentFavUseCaseProvider)));
+
+class DigifitEquipmentFav {
+  RefreshTokenProvider refreshTokenProvider;
+  TokenStatus tokenStatus;
+  DigifitEquipmentFavUseCase digifitEquipmentFavUseCase;
+
+  DigifitEquipmentFav(
+      {required this.refreshTokenProvider,
+      required this.tokenStatus,
+      required this.digifitEquipmentFavUseCase});
+
+  Future<void> changeEquipmentFavStatus(
+      {required Future<void> Function(bool) onFavStatusChange,
+      required DigifitEquipmentFavParams params}) async {
+    try {
+      final status = tokenStatus.isAccessTokenExpired();
+
+      if (status) {
+        refreshTokenProvider.getNewToken(
+            onError: () {},
+            onSuccess: () async {
+              await _changeFav(
+                  params: params, onFavStatusChange: onFavStatusChange);
+            });
+      } else {
+        await _changeFav(params: params, onFavStatusChange: onFavStatusChange);
+      }
+    } catch (error) {
+      rethrow;
+    }
+  }
+
+  _changeFav(
+      {required DigifitEquipmentFavParams params,
+      required Future<void> Function(bool) onFavStatusChange}) async {
+    try {
+      DigifitEquimentFavRequestModel requestModel =
+          DigifitEquimentFavRequestModel(
+              isFavorite: params.isFavorite,
+              equipmentId: params.equipmentId,
+              locationId: params.locationId);
+
+      DigifitEquipmentFavResponseModel responseModel =
+          DigifitEquipmentFavResponseModel();
+
+      final response =
+          await digifitEquipmentFavUseCase.call(requestModel, responseModel);
+
+      response.fold((l) {
+        debugPrint('exception while marking fav = ${l.toString()}');
+      }, (r) async{
+        final res = r as DigifitEquipmentFavResponseModel;
+        if(params.isFavorite) {
+          onFavStatusChange(false);
+        } else {
+          onFavStatusChange(true);
+        }
+      });
+    } catch (error) {
+      rethrow;
+    }
+  }
+}
+
+class DigifitEquipmentFavParams {
+  bool isFavorite;
+  int equipmentId;
+  int locationId;
+
+  DigifitEquipmentFavParams({
+    required this.isFavorite,
+    required this.equipmentId,
+    required this.locationId,
+  });
+}
