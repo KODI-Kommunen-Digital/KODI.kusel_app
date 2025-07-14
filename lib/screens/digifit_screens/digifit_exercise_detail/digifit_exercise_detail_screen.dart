ab7552478be8a4603c8e4d55145d3807c73c1467
Naman Khanaa <naman.khanaa@fiftyfivetech.io>
2025-07-14 19:42:53 +0530

    Refactor: Handle the UI timer issue.

--- lib/screens/digifit_screens/digifit_exercise_detail/digifit_exercise_detail_screen.dart
+++ lib/screens/digifit_screens/digifit_exercise_detail/digifit_exercise_detail_screen.dart
@@ -632,25 +632,32 @@ class _DigifitExerciseDetailScreenState
     ref
         .read(digifitExerciseDetailsControllerProvider(equipmentId).notifier)
         .updateTimerStatus(TimerState.start);
-    final state = ref.watch(digifitExerciseDetailsControllerProvider(equipmentId));
-    int secondsLeft = state.time;
-    if (state.remainingPauseSecond > 0) {
-      secondsLeft = state.remainingPauseSecond;
-    }
 
-    timer = Timer.periodic(Duration(seconds: 1), (time) {
+    final state =
+        ref.watch(digifitExerciseDetailsControllerProvider(equipmentId));
 
-      final equipmentId = widget.digifitExerciseDetailsParams.station.id ?? 0;
+    int secondsLeft = state.remainingPauseSecond > 0
+        ? state.remainingPauseSecond
+        : state.time;
 
+    ref
+        .read(digifitExerciseDetailsControllerProvider(equipmentId).notifier)
+        .updateRemainingSeconds(secondsLeft);
+
+    timer = Timer.periodic(const Duration(seconds: 1), (time) {
       if (secondsLeft == 0) {
         pauseTimer();
+
         ref
-            .read(digifitExerciseDetailsControllerProvider(equipmentId).notifier)
+            .read(
+                digifitExerciseDetailsControllerProvider(equipmentId).notifier)
             .updateIsReadyToSubmitSetVisibility(true);
       } else {
         secondsLeft--;
+
         ref
-            .read(digifitExerciseDetailsControllerProvider(equipmentId).notifier)
+            .read(
+                digifitExerciseDetailsControllerProvider(equipmentId).notifier)
             .updateRemainingSeconds(secondsLeft);
       }
     });
