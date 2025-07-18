75a7ce3aefaab1ca0da148d033ed12d8eccfbb9e
dev6ak <dev6@heidi-app.com>
2025-07-18 17:43:38 +0530

    fix: Update time for digifit_exercise_screen.

--- lib/screens/digifit_screens/digifit_exercise_detail/digifit_exercise_details_state.dart
+++ lib/screens/digifit_screens/digifit_exercise_detail/digifit_exercise_details_state.dart
@@ -55,7 +55,7 @@ class DigifitExerciseDetailsState {
         isScannerVisible: true,
         locationId: 0,
         timerState: TimerState.start,
-        time: 10,
+        time: 60,
         isNetworkAvailable: true,
         digifitUpdateExerciseRequestModel: null,
         createdAt: '',
