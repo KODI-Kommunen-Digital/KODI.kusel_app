851341701c07e6e2986eac3a1fbedb409a508408
dev6ak <dev6@heidi-app.com>
2025-08-29 16:09:36 +0530

    update: Update the minSdkVersion to 23 and BottomAppBarThemeData to BottomAppBarTheme and removed the unused imports.

--- domain/lib/usecase/search/search_usecase.dart
+++ domain/lib/usecase/search/search_usecase.dart
@@ -1,8 +1,6 @@
 import 'package:core/base_model.dart';
 import 'package:dartz/dartz.dart';
-import 'package:data/params/listings_params.dart';
 import 'package:data/repo_impl/search/search_repo_impl.dart';
-import 'package:domain/model/empty_request.dart';
 import 'package:domain/model/request_model/listings/search_request_model.dart';
 import 'package:domain/usecase/usecase.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
